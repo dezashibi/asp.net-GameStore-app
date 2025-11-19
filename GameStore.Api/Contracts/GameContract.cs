d5954aa97ce44558115c39cb015e46b428e24b5b
Navid Dezashibi <dezashibi@gmail.com>
2025-11-19 18:11:40 +0400

    introducing simple get requests for list and single object via contracts or data transfer objects
    - test using http file

---
+++ GameStore.Api/Contracts/GameContract.cs
@@ -0,0 +1,3 @@
+namespace GameStore.Api.Contracts;
+
+public record GameContract(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,19 @@
+using GameStore.Api.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameContract> games = [
+    new (1, "Sonic Racing Cross Worlds", "Racing", 69.99M, new DateOnly(2025, 9, 25)),
+    new (2, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new (3, "Final Fantasy XIV", "RPG", 59.99M, new DateOnly(2010, 9, 30)),
+    new (4, "FIFA 23", "Sports", 69.99M, new DateOnly(2022, 9, 27)),
+];
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
