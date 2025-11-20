4e7c66d2f8eb4fda174db7c27164b674a6666177
Navid Dezashibi <dezashibi@gmail.com>
2025-11-20 17:07:02 +0400

    use dependency injection for games and genres clients

--- GameStore.Frontend/Program.cs
+++ GameStore.Frontend/Program.cs
@@ -1,8 +1,11 @@
+using GameStore.Frontend.Clients;
 using GameStore.Frontend.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents();
+builder.Services.AddSingleton<GamesClient>();
+builder.Services.AddSingleton<GenresClient>();
 
 var app = builder.Build();
 
