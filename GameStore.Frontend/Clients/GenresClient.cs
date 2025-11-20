4c391d4168553680946d4f3aeea54d32773ae08c
Navid Dezashibi <dezashibi@gmail.com>
2025-11-20 16:13:02 +0400

    loading genres in edit form
    - add clients and models in imports

---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,20 @@
+using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] _genres =
+    [
+        new() { Id = 1, Name = "Fighting" },
+        new() { Id = 2, Name = "RPG" },
+        new() { Id = 3, Name = "Sports" },
+        new() { Id = 4, Name = "Racing" },
+        new() { Id = 5, Name = "Kids and Family" }
+    ];
+
+    public Genre[] GetGenres()
+    {
+        return _genres;
+    }
+}
