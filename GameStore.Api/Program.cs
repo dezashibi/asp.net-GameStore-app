55ab1905031f7c5dabcf63dd3aa0b199a74bed58
Navid Dezashibi <dezashibi@gmail.com>
2025-11-20 11:34:44 +0400

    setup entity framework core with sqlite
    - setup connection string in appsettings
    - install corresponding nuget packages
    - add Game and Genre entities
    - create migrations and perform them

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,11 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connStr = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connStr);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
